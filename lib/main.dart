d078dd93e958d8c0719b99cab6690f18ed74ac8d
Ifaz Islam <ifaz@example.com>
2026-01-31 17:04:07 +0600

    Worcable

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Food For Life'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Welcome to Food For Life'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        print('Button clicked!');
-      },
-      child: Center(child: Text('Click', style: TextStyle(fontSize: 12))),
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Food For Life'), centerTitle: true),
+      body: Center(child: Text('Welcome to Food For Life')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button clicked!');
+        },
+        child: Center(child: Text('Go', style: TextStyle(fontSize: 12))),
+      ),
     ),
   ),
-));
+);
